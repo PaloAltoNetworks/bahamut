ebda0c699c849e0f488bbdfbccf0529f2043488e
Antoine Mercadal <antoine.mercadal@gmail.com>
2016-08-03 13:54:42 -0700

    new: added doc.go

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Package bahamut contains everything needed to build a fast and secure server
+// based on a set of Monolithe Specifications.
+//
+// Bahamut uses a Monolithe plugins to generate a set of Monolithe Specifications
+// into the necessary routes and handlers. You will just need to write various processors
+// to handle the business logic and storage.
+//
+// Bahamut works with an Elemental generated model, and you can use any storage engine you like.
+// It works best with Manipulate.
+package bahamut
