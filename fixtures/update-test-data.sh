57553899bf9c88ae6ccb0d48f5f7c4e3f91e1231
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-03-07 16:30:46 -0800

    support for new elemental

--- fixtures/update-test-data.sh
+++ fixtures/update-test-data.sh
@@ -17,12 +17,12 @@ openssl genrsa -out client-key.pem 4096 \
 monogen -f ./specs -L elemental
 
 echo -n > ./data_test.go
-cat ./codegen/elemental/1.0/list.go >> ./data_test.go
-cat ./codegen/elemental/1.0/task.go | (read; read; read; read; cat) >> ./data_test.go
-cat ./codegen/elemental/1.0/root.go | (read; read; read; read; read; read; cat) >> ./data_test.go
-cat ./codegen/elemental/1.0/user.go | (read; read;read; read; cat) >> ./data_test.go
-cat ./codegen/elemental/1.0/relationships_registry.go | (read; read; read; cat) >> ./data_test.go
-cat ./codegen/elemental/1.0/identities_registry.go | (read; read; read; cat) >> ./data_test.go
+cat ./codegen/elemental/list.go >> ./data_test.go
+cat ./codegen/elemental/task.go | (read; read; read; read; cat) >> ./data_test.go
+cat ./codegen/elemental/root.go | (read; read; read; read; read; read; cat) >> ./data_test.go
+cat ./codegen/elemental/user.go | (read; read;read; read; cat) >> ./data_test.go
+cat ./codegen/elemental/relationships_registry.go | (read; read; read; cat) >> ./data_test.go
+cat ./codegen/elemental/identities_registry.go | (read; read; read; cat) >> ./data_test.go
 cat >> ./data_test.go << EOF
 
 var UnmarshalableListIdentity = elemental.Identity{Name: "list", Category: "lists"}
