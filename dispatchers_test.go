1e729e903b64492790db150a149ca48a0830e188
Christophe Serafin <christophe.serafin@gmail.com>
2017-05-24 17:34:36 -0700

    New: added tests on dispatchRetrieveOperation

--- dispatchers_test.go
+++ dispatchers_test.go
@@ -35,12 +35,16 @@ func (p *FakeIdentifiable) SetIdentifier(identifier string) {
 	p.identifier = identifier
 }
 
-// FakeRetrieveManyProcessor
-type FakeRetrieveManyProcessor struct {
+// FakeCompleteProcessor
+type FakeCompleteProcessor struct {
 	err error
 }
 
-func (p *FakeRetrieveManyProcessor) ProcessRetrieveMany(*Context) error {
+func (p *FakeCompleteProcessor) ProcessRetrieveMany(*Context) error {
+	return p.err
+}
+
+func (p *FakeCompleteProcessor) ProcessRetrieve(*Context) error {
 	return p.err
 }
 
@@ -51,7 +55,7 @@ func TestDispatchers_dispatchRetrieveManyOperation(t *testing.T) {
 		request := elemental.NewRequest()
 
 		processorFinder := func(identity elemental.Identity) (Processor, error) {
-			return &FakeRetrieveManyProcessor{}, nil
+			return &FakeCompleteProcessor{}, nil
 		}
 
 		factory := func(identity string) elemental.Identifiable {
@@ -75,16 +79,13 @@ func TestDispatchers_dispatchRetrieveManyOperation(t *testing.T) {
 		request := elemental.NewRequest()
 
 		processorFinder := func(identity elemental.Identity) (Processor, error) {
-			return &FakeRetrieveManyProcessor{
+			return &FakeCompleteProcessor{
 				err: elemental.NewError("Error", "Bad request.", "bahamut-test", http.StatusBadRequest),
 			}, nil
 		}
 
 		factory := func(identity string) elemental.Identifiable {
-			return &FakeIdentifiable{
-				identity:   identity,
-				identifier: "FakeIdentifier",
-			}
+			return &FakeIdentifiable{}
 		}
 
 		auditer := &FakeAuditer{}
@@ -128,7 +129,7 @@ func TestDispatchers_dispatchRetrieveManyOperation(t *testing.T) {
 		})
 	})
 
-	Convey("Given I have an authenticator that is not authenticated", t, func() {
+	Convey("Given I have a processor that handle ProcessRetrieveMany function and an authenticator that is not authenticated", t, func() {
 		request := elemental.NewRequest()
 
 		processorFinder := func(identity elemental.Identity) (Processor, error) {
@@ -158,7 +159,7 @@ func TestDispatchers_dispatchRetrieveManyOperation(t *testing.T) {
 		})
 	})
 
-	Convey("Given I have an authorizer that is not authorize", t, func() {
+	Convey("Given I have a processor that handle ProcessRetrieveMany function and an authorizer that is not authorize", t, func() {
 		request := elemental.NewRequest()
 
 		processorFinder := func(identity elemental.Identity) (Processor, error) {
@@ -192,3 +193,199 @@ func TestDispatchers_dispatchRetrieveManyOperation(t *testing.T) {
 		})
 	})
 }
+
+// TestDispatchers_dispatchRetrieveManyOperation tests dispatchRetrieveManyOperation method
+func TestDispatchers_dispatchRetrieveOperation(t *testing.T) {
+
+	Convey("Given I have a processor that handle ProcessRetrieve function", t, func() {
+		request := elemental.NewRequest()
+
+		processorFinder := func(identity elemental.Identity) (Processor, error) {
+			return &FakeCompleteProcessor{}, nil
+		}
+
+		factory := func(identity string) elemental.Identifiable {
+			return &FakeIdentifiable{}
+		}
+
+		auditer := &FakeAuditer{}
+
+		ctx, err := dispatchRetrieveOperation(request, processorFinder, factory, nil, nil, nil, auditer)
+
+		expectedNbCalls := 1
+
+		Convey("Then I should have no error and context should be initiated", func() {
+			So(err, ShouldBeNil)
+			So(ctx, ShouldNotBeNil)
+			So(auditer.nbCalls, ShouldEqual, expectedNbCalls)
+		})
+	})
+
+	Convey("Given I have a processor that handle ProcessRetrieve function with error", t, func() {
+		request := elemental.NewRequest()
+
+		processorFinder := func(identity elemental.Identity) (Processor, error) {
+			return &FakeCompleteProcessor{
+				err: elemental.NewError("Error", "Bad request.", "bahamut-test", http.StatusBadRequest),
+			}, nil
+		}
+
+		factory := func(identity string) elemental.Identifiable {
+			return &FakeIdentifiable{}
+		}
+
+		auditer := &FakeAuditer{}
+
+		ctx, err := dispatchRetrieveOperation(request, processorFinder, factory, nil, nil, nil, auditer)
+
+		expectedError := "error 400 (bahamut-test): Error: Bad request."
+		expectedNbCalls := 1
+
+		Convey("Then I should get a bahamut error and no context", func() {
+			So(err.Error(), ShouldEqual, expectedError)
+			So(ctx, ShouldBeNil)
+			So(auditer.nbCalls, ShouldEqual, expectedNbCalls)
+		})
+	})
+
+	Convey("Given I have a processor that does not handle ProcessRetrieve function", t, func() {
+		request := elemental.NewRequest()
+		request.Operation = elemental.OperationRetrieveMany
+		request.Identity = elemental.MakeIdentity("Fake", "Test")
+
+		processorFinder := func(identity elemental.Identity) (Processor, error) {
+			return &FakeProcessor{}, nil
+		}
+
+		factory := func(identity string) elemental.Identifiable {
+			return &FakeIdentifiable{}
+		}
+
+		auditer := &FakeAuditer{}
+
+		expectedError := "error 501 (bahamut): Not implemented: No handler for operation retrieve-many on Fake"
+		expectedNbCalls := 1
+
+		ctx, err := dispatchRetrieveOperation(request, processorFinder, factory, nil, nil, nil, auditer)
+
+		Convey("Then I should get a bahamut error and no context", func() {
+			So(err.Error(), ShouldEqual, expectedError)
+			So(ctx, ShouldBeNil)
+			So(auditer.nbCalls, ShouldEqual, expectedNbCalls)
+		})
+	})
+
+	Convey("Given I have a processor that does not handle ProcessRetrieve function and an authenticator that is not authenticated", t, func() {
+		request := elemental.NewRequest()
+
+		processorFinder := func(identity elemental.Identity) (Processor, error) {
+			return &FakeProcessor{}, nil
+		}
+
+		factory := func(identity string) elemental.Identifiable {
+			return &FakeIdentifiable{}
+		}
+
+		authenticator := &Auth{
+			errored: true,
+			err:     elemental.NewError("Error", "Authenticator does not authenticate.", "bahamut-test", http.StatusInternalServerError),
+		}
+
+		auditer := &FakeAuditer{}
+
+		expectedError := "error 500 (bahamut-test): Error: Authenticator does not authenticate."
+		expectedNbCalls := 1
+
+		ctx, err := dispatchRetrieveOperation(request, processorFinder, factory, authenticator, nil, nil, auditer)
+
+		Convey("Then I should get a bahamut error and no context", func() {
+			So(err.Error(), ShouldEqual, expectedError)
+			So(ctx, ShouldBeNil)
+			So(auditer.nbCalls, ShouldEqual, expectedNbCalls)
+		})
+	})
+
+	Convey("Given I have a processor that does not handle ProcessRetrieve function and an authorizer that is not authorize", t, func() {
+		request := elemental.NewRequest()
+
+		processorFinder := func(identity elemental.Identity) (Processor, error) {
+			return &FakeProcessor{}, nil
+		}
+
+		factory := func(identity string) elemental.Identifiable {
+			return &FakeIdentifiable{}
+		}
+
+		authenticator := &Auth{
+			authenticated: true,
+		}
+
+		authorizer := &Auth{
+			errored: true,
+			err:     elemental.NewError("Error", "Authorizer does not authorize.", "bahamut-test", http.StatusInternalServerError),
+		}
+
+		auditer := &FakeAuditer{}
+
+		expectedError := "error 500 (bahamut-test): Error: Authorizer does not authorize."
+		expectedNbCalls := 1
+
+		ctx, err := dispatchRetrieveOperation(request, processorFinder, factory, authenticator, authorizer, nil, auditer)
+
+		Convey("Then I should get a bahamut error and no context", func() {
+			So(err.Error(), ShouldEqual, expectedError)
+			So(ctx, ShouldBeNil)
+			So(auditer.nbCalls, ShouldEqual, expectedNbCalls)
+		})
+	})
+}
+
+// func dispatchRetrieveOperation(
+// 	request *elemental.Request,
+// 	processorFinder processorFinder,
+// 	factory elemental.IdentifiableFactory,
+// 	authenticator RequestAuthenticator,
+// 	authorizer Authorizer,
+// 	pusher eventPusher,
+// 	auditer Auditer,
+// ) (*Context, error) {
+//
+// 	ctx := NewContext()
+// 	if err := ctx.ReadElementalRequest(request); err != nil {
+// 		return nil, err
+// 	}
+//
+// 	ctx.Request.StartTracing()
+// 	defer ctx.Request.FinishTracing()
+//
+// 	if err := CheckAuthentication(authenticator, ctx); err != nil {
+// 		audit(auditer, ctx, err)
+// 		return nil, err
+// 	}
+//
+// 	if err := CheckAuthorization(authorizer, ctx); err != nil {
+// 		audit(auditer, ctx, err)
+// 		return nil, err
+// 	}
+//
+// 	proc, _ := processorFinder(request.Identity)
+//
+// 	if _, ok := proc.(RetrieveProcessor); !ok {
+// 		err := notImplementedErr(request)
+// 		audit(auditer, ctx, err)
+// 		return nil, err
+// 	}
+//
+// 	if err := proc.(RetrieveProcessor).ProcessRetrieve(ctx); err != nil {
+// 		audit(auditer, ctx, err)
+// 		return nil, err
+// 	}
+//
+// 	if ctx.HasEvents() {
+// 		pusher(ctx.Events()...)
+// 	}
+//
+// 	audit(auditer, ctx, nil)
+//
+// 	return ctx, nil
+// }
