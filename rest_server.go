c23948d59945cb18b059e5e906bf2a4c4298e971
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-07-12 16:18:28 -0700

    fixed/tls: declare prefered curves

--- rest_server.go
+++ rest_server.go
@@ -58,6 +58,7 @@ func (a *restServer) createSecureHTTPServer(address string) *http.Server {
 		MinVersion:               tls.VersionTLS12,
 		SessionTicketsDisabled:   true,
 		PreferServerCipherSuites: true,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
