5cf980daf74088898920b9cf2922c6624d8e2405
primalmotion <208711+primalmotion@users.noreply.github.com>
2022-06-08 14:56:46 -0700

    gateway/sourceextractor: add way to consider cookie. (#159)
    This patch adds a new function NewDefaultSourceExtractor(string) that
    returns a SourceExtractor that can also look for a cookie with the given
    name to discriminate the source of a request.

--- gateway/extractors_test.go
+++ gateway/extractors_test.go
@@ -18,7 +18,7 @@ func Test_defaultSourceExtractor_ExtractSource(t *testing.T) {
 	}{
 		{
 			"no header",
-			defaultSourceExtractor{},
+			NewDefaultSourceExtractor("").(defaultSourceExtractor),
 			args{
 				&http.Request{Header: http.Header{}},
 			},
@@ -27,9 +27,74 @@ func Test_defaultSourceExtractor_ExtractSource(t *testing.T) {
 		},
 		{
 			"header",
-			defaultSourceExtractor{},
+			NewDefaultSourceExtractor("").(defaultSourceExtractor),
 			args{
 				&http.Request{Header: http.Header{"Authorization": {"Bearer X"}}},
+			},
+			"763578776710062675",
+			false,
+		},
+		{
+			"cookie",
+			NewDefaultSourceExtractor("X-Token").(defaultSourceExtractor),
+			args{
+				func() *http.Request {
+					r := &http.Request{Header: http.Header{}}
+					r.AddCookie(&http.Cookie{Name: "X-Token", Value: "X"})
+					return r
+				}(),
+			},
+			"15784302077936868069",
+			false,
+		},
+		{
+			"cookie with no configured authCookieName",
+			NewDefaultSourceExtractor("").(defaultSourceExtractor),
+			args{
+				func() *http.Request {
+					r := &http.Request{Header: http.Header{}}
+					r.AddCookie(&http.Cookie{Name: "X-Token", Value: "X"})
+					return r
+				}(),
+			},
+			"default",
+			false,
+		},
+		{
+			"empty cookie",
+			NewDefaultSourceExtractor("X-Token").(defaultSourceExtractor),
+			args{
+				func() *http.Request {
+					r := &http.Request{Header: http.Header{}}
+					r.AddCookie(&http.Cookie{})
+					return r
+				}(),
+			},
+			"default",
+			false,
+		},
+		{
+			"cookie and header (cookie should have prio)",
+			NewDefaultSourceExtractor("X-Token").(defaultSourceExtractor),
+			args{
+				func() *http.Request {
+					r := &http.Request{Header: http.Header{"Authorization": {"Bearer X"}}}
+					r.AddCookie(&http.Cookie{Name: "X-Token", Value: "X"})
+					return r
+				}(),
+			},
+			"15784302077936868069",
+			false,
+		},
+		{
+			"empty cookie and header (header should fall back)",
+			NewDefaultSourceExtractor("X-Token").(defaultSourceExtractor),
+			args{
+				func() *http.Request {
+					r := &http.Request{Header: http.Header{"Authorization": {"Bearer X"}}}
+					r.AddCookie(&http.Cookie{})
+					return r
+				}(),
 			},
 			"763578776710062675",
 			false,
@@ -37,7 +102,7 @@ func Test_defaultSourceExtractor_ExtractSource(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			f := defaultSourceExtractor{}
+			f := tt.f
 			got, err := f.ExtractSource(tt.args.r)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("defaultSourceExtractor.ExtractSource() error = %v, wantErr %v", err, tt.wantErr)
