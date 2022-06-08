a43b6fd8934ed95394955f2b350af268c61690ea
primalmotion <208711+primalmotion@users.noreply.github.com>
2022-06-08 14:56:46 -0700

    gateway/sourceextractor: add way to consider cookie. (#159)
    This patch adds a new function NewDefaultSourceExtractor(string) that
    returns a SourceExtractor that can also look for a cookie with the given
    name to discriminate the source of a request.

--- gateway/extractors.go
+++ gateway/extractors.go
@@ -7,16 +7,44 @@ import (
 	"github.com/cespare/xxhash"
 )
 
-type defaultSourceExtractor struct{}
+type defaultSourceExtractor struct {
+	authCookieName string
+}
+
+// NewDefaultSourceExtractor returns a default SourceExtractor.
+// A source extractor will discriminate the source of a request
+// based on a hash of its authentication string.
+// It will first use an eventual cookie with the given name,
+// then use then use the Authorization header.
+// If both are empty, the bucket key will be 'default'.
+// If authCookieName is empty, only the value of the Authorization
+// header will be taken into account.
+func NewDefaultSourceExtractor(authCookieName string) SourceExtractor {
+	return defaultSourceExtractor{
+		authCookieName: authCookieName,
+	}
+}
 
 func (f defaultSourceExtractor) ExtractSource(r *http.Request) (string, error) {
 
+	var v string
 	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
+
+	var authCookie *http.Cookie
+	if f.authCookieName != "" {
+		authCookie, _ = r.Cookie(f.authCookieName)
+	}
+
+	switch {
+	case authCookie != nil && authCookie.Value != "":
+		v = authCookie.Value
+	case authHeader != "":
+		v = authHeader
+	default:
 		return "default", nil
 	}
 
-	return fmt.Sprintf("%d", xxhash.Sum64([]byte(authHeader))), nil
+	return fmt.Sprintf("%d", xxhash.Sum64([]byte(v))), nil
 }
 
 type defaultTCPSourceExtractor struct{}
