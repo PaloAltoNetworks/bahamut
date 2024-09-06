500f3432cbf22254ad3797d45275653e1445c15a
Brian Onn <bonn@paloaltonetworks.com>
2024-09-06 14:07:54 -0700

    fix data race and remove oxy v1

--- gateway/errors_test.go
+++ gateway/errors_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/mailgun/multibuf"
 	// nolint:revive // Allow dot imports for readability in tests
 	. "github.com/smartystreets/goconvey/convey"
-	"github.com/vulcand/oxy/ratelimit"
 	"github.com/vulcand/oxy/v2/connlimit"
+	"github.com/vulcand/oxy/v2/ratelimit"
 	"go.aporeto.io/elemental"
 )
 
